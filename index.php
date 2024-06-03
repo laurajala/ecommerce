4f5acc409cc0f095d927e2ac067bc544d24320e6
Laura <lauraq2000@hotmail.com>
2024-06-03 07:49:36 -0400

    autoload hcodebr classes

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
 
 require_once("vendor/autoload.php");
 
+use Hcode\DB\Sql;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$sql = new Hcode\DB\Sql();
+	$results =  $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
 
 });
 
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,5 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
-    'Hcode\\' => array($vendorDir . '/Hcode/php-classes/src'),
+    'Hcode\\' => array($vendorDir . '/hcodebr/php-classes/src'),
 );
